4c94dfde8453fc9c83a4418d62141f4c13b0ae90
Daniel Lombraña González <teleyinex@gmail.com>
2018-06-29 15:01:42 +0200

    Ensure redisdb

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ app = Flask(__name__)
 
 app.config.from_object(rq_dashboard.default_settings)
 app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
+app.config['REDIS_DB'] = settings.REDIS_DB
 
 
 socketio = SocketIO(app)
